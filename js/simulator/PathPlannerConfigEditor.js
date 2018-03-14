fa50774423acb2a4acf2585a34120809cdb2dda2
Matt Bradley <themattbradley@gmail.com>
2018-03-14 01:47:18 -0500

    Store planner config changes in local storage. Added button to restore defaults.

--- js/simulator/PathPlannerConfigEditor.js
+++ js/simulator/PathPlannerConfigEditor.js
@@ -1,15 +1,22 @@
 import Car from "../physics/Car.js";
 
-const defaultConfig = {
-  spatialHorizon: 100, // meters
-  centerlineStationInterval: 0.5, // meters
+const LOCAL_STORAGE_KEY = 'dashPathPlannerConfig';
 
+const internalConfig = {
   lattice: {
     numStations: 10,
     numLatitudes: 11,
     stationConnectivity: 3,
     latitudeConnectivity: 5
   },
+
+  dCurvatureMax: Car.MAX_STEER_SPEED / Car.WHEEL_BASE,
+  rearAxleToCenter: -Car.REAR_AXLE_POS
+};
+
+const defaultConfig = {
+  spatialHorizon: 100, // meters
+  centerlineStationInterval: 0.5, // meters
 
   xyGridCellSize: 0.1, // meters
   slGridCellSize: 0.05, // meters
@@ -31,32 +38,26 @@ const defaultConfig = {
   laneCostSlope: 3, // cost / meter
 
   stationReachDiscount: 10,
-  extraTimePenalty: 16,
+  extraTimePenalty: 64,
 
   hysteresisDiscount: 1,
 
   speedLimit: 20, // m/s
   speedLimitPenalty: 2,
 
-  hardAccelerationPenalty: 1,
+  hardAccelerationPenalty: 4,
   hardDecelerationPenalty: 2,
 
   lateralAccelerationLimit: 3, // m/s^2
   softLateralAccelerationPenalty: 1,
   linearLateralAccelerationPenalty: 0.1,
 
-  accelerationChangePenalty: 0.5,
-
-  dCurvatureMax: Car.MAX_STEER_SPEED / Car.WHEEL_BASE,
-
-  rearAxleToCenter: -Car.REAR_AXLE_POS
+  accelerationChangePenalty: 0.5
 };
-
-const ignoreKeys = ['rearAxleToCenter', 'dCurvatureMax', 'lattice'];
 
 export default class PathPlannerConfigEditor {
   constructor() {
-    this.config = defaultConfig;
+    this._config = Object.assign({}, defaultConfig);
 
     this.showConfigBox = document.getElementById('show-config-box');
     this.configBox = document.getElementById('config-box-content');
@@ -64,10 +65,19 @@ export default class PathPlannerConfigEditor {
 
     this._setUpButtons();
 
-    const keys = Object.keys(this.config).filter(k => !ignoreKeys.includes(k)).sort();
-    
-    for (const key of keys)
-      this.configForm.appendChild(this._createConfigField(key, this.config[key]));
+    let storedConfig = {};
+    try {
+      storedConfig = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+    } catch (e) {}
+
+    for (const key of Object.keys(this._config)) {
+      if (storedConfig[key] !== undefined) this._config[key] = storedConfig[key];
+      this.configForm.appendChild(this._createConfigField(key, this._config[key]));
+    }
+  }
+
+  get config() {
+    return Object.assign({}, this._config, internalConfig);
   }
 
   _setUpButtons() {
@@ -82,6 +92,7 @@ export default class PathPlannerConfigEditor {
     });
 
     document.getElementById('save-config-button').addEventListener('click', this._saveConfigFields.bind(this));
+    document.getElementById('restore-defaults-config-button').addEventListener('click', this._restoreDefaults.bind(this));
   }
 
   _createConfigField(key, value) {
@@ -93,7 +104,7 @@ export default class PathPlannerConfigEditor {
           <div class="field-body">
               <div class="field">
                   <div class="control" style="margin-right: 16px;">
-                      <input id="config-field-${key}" name="${key}" class="input is-small" type="text" style="width: 60px" value="${value}" />
+                      <input id="config-field-${key}" name="${key}" class="input is-small ${value != defaultConfig[key] ? 'is-warning' : ''}" type="text" style="width: 60px; border-width: 2px;" value="${value}" />
                   </div>
               </div>
           </div>
@@ -107,7 +118,32 @@ export default class PathPlannerConfigEditor {
   _saveConfigFields() {
     const formData = new FormData(this.configForm);
 
-    for (const [k, v] of formData.entries())
-      this.config[k] = Number.parseFloat(v);
+    for (const [k, v] of formData.entries()) {
+      this._config[k] = Number.parseFloat(v);
+
+      const fieldDom = document.getElementById(`config-field-${k}`);
+      if (v == defaultConfig[k])
+        fieldDom.classList.remove('is-warning');
+      else
+        fieldDom.classList.add('is-warning');
+    }
+
+    try {
+      window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this._config));
+    } catch (e) {}
+  }
+
+  _restoreDefaults() {
+    this._config = defaultConfig;
+
+    try {
+      window.localStorage.removeItem(LOCAL_STORAGE_KEY);
+    } catch (e) {}
+
+    while (this.configForm.firstChild)
+      this.configForm.removeChild(this.configForm.firstChild);
+
+    for (const key of Object.keys(this._config))
+      this.configForm.appendChild(this._createConfigField(key, this._config[key]));
   }
 }
