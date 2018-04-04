0e1e0652db4c32c37810b4b8f705ad12b0e8a968
Matt Bradley <themattbradley@gmail.com>
2018-04-03 20:30:36 -0500

    Prod webpack config and dist/.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
 const webpack = require('webpack');
 const WrapperPlugin = require('wrapper-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
     PathPlannerWorker: './workers/PathPlannerWorker.js',
     Dash: './js/Dash.js'
   },
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   plugins: [
     new WrapperPlugin({
       test: /PathPlannerWorker.js/,
       header: 'function dash_initPathPlannerWorker() {',
       footer: '} if (typeof(window) === undefined) dash_initPathPlannerWorker();'
-    })
+    }),
+    new UglifyJSPlugin({ sourceMap: true })
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
