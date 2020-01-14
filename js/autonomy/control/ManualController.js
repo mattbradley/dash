4d50faec116a73554e482f2800e39ed8459ee438
Wolfgang Fahl <wf@bitplan.com>
2020-01-14 18:38:42 +0100

    fixes #6

--- js/autonomy/control/ManualController.js
+++ js/autonomy/control/ManualController.js
@@ -3,21 +3,24 @@ export default class ManualController {
     this.carKeys = { forward: false, backward: false, left: false, right: false, brake: false };
 
     document.addEventListener('keydown', event => {
+      // ignore Command key ⌘
+	  	if (event.metaKey)
+		  	return;
       switch (event.key) {
-        case 'w': case 'W': this.carKeys.forward = true; break;
-        case 's': case 'S': this.carKeys.backward = true; break;
-        case 'a': case 'A': this.carKeys.left = true; break;
-        case 'd': case 'D': this.carKeys.right = true; break;
+        case 'w': case 'W': case 'ArrowUp': this.carKeys.forward = true; break;
+        case 's': case 'S': case 'ArrowDown': this.carKeys.backward = true; break;
+        case 'a': case 'A': case 'ArrowLeft': this.carKeys.left = true; break;
+        case 'd': case 'D': case 'ArrowRight': this.carKeys.right = true; break;
         case ' ': this.carKeys.brake = true; break;
       }
     });
 
     document.addEventListener('keyup', event => {
       switch (event.key) {
-        case 'w': case 'W': this.carKeys.forward = false; break;
-        case 's': case 'S': this.carKeys.backward = false; break;
-        case 'a': case 'A': this.carKeys.left = false; break;
-        case 'd': case 'D': this.carKeys.right = false; break;
+        case 'w': case 'W': case 'ArrowUp': this.carKeys.forward = false; break;
+        case 's': case 'S': case 'ArrowDown': this.carKeys.backward = false; break;
+        case 'a': case 'A': case 'ArrowLeft': this.carKeys.left = false; break;
+        case 'd': case 'D': case 'ArrowRight': this.carKeys.right = false; break;
         case ' ': this.carKeys.brake = false; break;
       }
     });
